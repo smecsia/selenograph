5e419b1cb17218e9e21a3094341b5e7d0bf46b61
Ilya Sadykov <smecsia@gmail.com>
2016-04-28 08:53:44 +0300

    Sessions update in bulk

--- service/src/main/java/ru/qatools/selenograph/gridrouter/QuotaStatsAggregator.java
+++ service/src/main/java/ru/qatools/selenograph/gridrouter/QuotaStatsAggregator.java
@@ -67,13 +67,12 @@ public class QuotaStatsAggregator {
             graphite.produce(new GraphiteValue(prefix + ".stats.raw", state.getRaw(), timestamp));
             graphite.produce(new GraphiteValue(prefix + ".stats.max", state.getMax(), timestamp));
             graphite.produce(new GraphiteValue(prefix + ".stats.avg", state.getAvg(), timestamp));
-            Integer currentCount = current.get(fromBrowserString(entry.getKey()));
-            if (currentCount == null) {
-                currentCount = 0;
+            final Integer currentCount = current.get(fromBrowserString(entry.getKey()));
+            if(currentCount != null){
+                state.setMax(currentCount);
+                state.setAvg(currentCount);
+                state.setRaw(currentCount);
             }
-            state.setMax(currentCount);
-            state.setAvg(currentCount);
-            state.setRaw(currentCount);
         });
     }
 }
